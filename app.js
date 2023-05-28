ef78be47df439f4ef1cbc4793e6dbf859116288c
SudipSinghKhati <sudipkhati2043@gmail.com>
2023-05-28 08:35:40 +0545

    changing url

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers)
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Making Api</title></head>')
-    res.write('<body><h1>Making Api using Node js</h1></body>')
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write(
+      '<body><form action ="/message" method ="POST"><input type = "text" name="message"><button type = ""submit>submit</button></form></body>'
+    );
     res.write("</html>");
-    res.end();
-    
-
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Making Api</title></head>");
+  res.write("<body><h1>Making Api using Node js</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
-server.listen(3001)
+server.listen(3001);
